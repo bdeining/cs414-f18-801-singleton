562c88ff816c4557cfbf9e78fbf59a20a2602689
bdeining <benjamin.deininger@connexta.com>
2018-10-16 07:52:47 -0600

    Rest Working

--- rest/src/main/java/edu/colostate/cs/cs414/p3/bdeining/rest/RestService.java
+++ rest/src/main/java/edu/colostate/cs/cs414/p3/bdeining/rest/RestService.java
@@ -1,12 +1,18 @@
 package edu.colostate.cs.cs414.p3.bdeining.rest;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.google.gson.Gson;
 
 import edu.colostate.cs.cs414.p3.bdeining.api.MySqlHandler;
 import edu.colostate.cs.cs414.p3.bdeining.api.Trainer;
@@ -16,16 +22,29 @@ public class RestService {
 
     private MySqlHandler mySqlHandler;
 
-    public void setMySqlHandler(MySqlHandler mySqlHandler) {
+    private Gson gson = new Gson();
+
+    public RestService(MySqlHandler mySqlHandler) {
         this.mySqlHandler = mySqlHandler;
     }
 
     @GET
-    @Produces({MediaType.APPLICATION_JSON})
-    @Path("trainer")
-    public List<Trainer> getTrainer() {
-        return mySqlHandler.getTrainers();
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/trainer")
+    public Response getTrainer() {
+        return Response.ok().entity(mySqlHandler.getTrainers()).build();
     }
 
 
+    @PUT
+    @Path("/trainer")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createTrainer(InputStream requestBody){
+
+        Trainer trainer = gson.fromJson(new InputStreamReader(requestBody), Trainer.class);
+
+        mySqlHandler.addTrainer(trainer);
+
+    return Response.ok().build();
+    }
 }
